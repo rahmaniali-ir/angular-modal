e4e9eff51274ad435fe9c929f0997ae911b337d3
Ali Rahmani <alirahmani.prog@gmail.com>
2022-12-31 12:09:55 +0330

    export modal module

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of modal
  */
 
-export * from './modal/service/modal.service';
-export * from './modal/service/active-modal.service';
+export * from "./modal/service/modal.service"
+export * from "./modal/service/active-modal.service"
+export * from "./modal/modal.module"
